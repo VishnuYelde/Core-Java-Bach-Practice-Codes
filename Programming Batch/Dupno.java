65914872618c592f88681c9236fae491d16ecea8
Vishnu Yelde <vishnuyelde@gmail.com>
2026-03-20 22:01:14 +0530

    Duplicate No practice

---
+++ Programming Batch/Dupno.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Dupno {
+    public static void main(String[] args) {
+        int[] arr = { 10, 20, 25, 33, 55, 10, 25 };
+
+        System.out.println(dupNo(arr));
+    }
+
+    public static Set<Integer> dupNo(int[] arr) {
+
+        // empty set, only unique element will be added
+        HashSet<Integer> set = new HashSet<>();
+
+        // duplicate element will be added
+        HashSet<Integer> dupHashSet = new HashSet<>();
+
+        for (int num : arr) {
+            if (!set.add(num)) { // same element not added, only unique element is added
+                dupHashSet.add(num); // duplicate element is added
+            }
+        }
+        return dupHashSet;
+    }
+}
