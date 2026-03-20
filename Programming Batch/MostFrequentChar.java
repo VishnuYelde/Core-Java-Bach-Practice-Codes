afa2421281ea299fb8a6575d568edc8257a3b9e9
Vishnu Yelde <vishnuyelde@gmail.com>
2026-03-20 20:10:36 +0530

    most Frequent Char practice

---
+++ Programming Batch/MostFrequentChar.java
@@ -0,0 +1,26 @@
+public class MostFrequentChar {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 21, 2, 2, 3, 53, 2, 36, 14, 4, 4, 3, 2, 4 };
+        String str = "programming";
+
+        System.out.println(mostFreqChar(str));
+    }
+
+    public static Character mostFreqChar(String str) {
+        int[] freq = new int[256];
+        for (int i = 0; i < str.length(); i++) {
+            freq[str.charAt(i)]++;
+        }
+
+        char maxChar = ' ';
+        int maxCount = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (freq[str.charAt(i)] > maxCount) {
+                maxCount = freq[str.charAt(i)];
+                maxChar = str.charAt(i);
+            }
+        }
+
+        return maxChar;
+    }
+}
